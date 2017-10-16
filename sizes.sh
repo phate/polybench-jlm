bbca79e2602214bae17aac1e229e0f947c2a29bd
Nico Reissmann <nico.reissmann@gmail.com>
2017-10-16 16:02:04 +0200

    Add script for retreiving code size of executable.

---
+++ sizes.sh
@@ -0,0 +1,57 @@
+#!	/bin/bash
+
+optflags=`cat optflags`
+echo "CC=clang-3.7" > config.mk
+echo "CPPFLAGS=-DPOLYBENCH_USE_C99_PROTO -DPOLYBENCH_TIME" >> config.mk
+echo "CFLAGS=-O0" >> config.mk
+echo "OPTFLAGS=$optflags" >> config.mk
+
+./compile_all.sh 1>&2
+
+declare -a kernels=(
+	"datamining/correlation/correlation"
+	"datamining/covariance/covariance"
+	"linear-algebra/blas/gemm/gemm"
+	"linear-algebra/blas/gemver/gemver"
+	"linear-algebra/blas/gesummv/gesummv"
+	"linear-algebra/blas/symm/symm"
+	"linear-algebra/blas/syr2k/syr2k"
+	"linear-algebra/blas/syrk/syrk"
+	"linear-algebra/blas/trmm/trmm"
+	"linear-algebra/kernels/2mm/2mm"
+	"linear-algebra/kernels/3mm/3mm"
+	"linear-algebra/kernels/atax/atax"
+	"linear-algebra/kernels/bicg/bicg"
+	"linear-algebra/kernels/doitgen/doitgen"
+	"linear-algebra/kernels/mvt/mvt"
+	"linear-algebra/solvers/cholesky/cholesky"
+	"linear-algebra/solvers/durbin/durbin"
+	"linear-algebra/solvers/gramschmidt/gramschmidt"
+	"linear-algebra/solvers/lu/lu"
+	"linear-algebra/solvers/ludcmp/ludcmp"
+	"linear-algebra/solvers/trisolv/trisolv"
+	"medley/deriche/deriche"
+	"medley/floyd-warshall/floyd-warshall"
+	"medley/nussinov/nussinov"
+	"stencils/adi/adi"
+	"stencils/fdtd-2d/fdtd-2d"
+	"stencils/heat-3d/heat-3d"
+	"stencils/jacobi-1d/jacobi-1d"
+	"stencils/jacobi-2d/jacobi-2d"
+	"stencils/seidel-2d/seidel-2d")
+
+
+echo "# $optflags"
+echo "# kernel Os O2 JLM"
+for kernel in "${kernels[@]}"; do
+	echo -n "$kernel "
+
+	OsSIZE=`size $kernel-Os | tail +2 | cut -f1 | xargs`
+	echo -n "$OsSIZE "
+
+	O2SIZE=`size $kernel-O2 | tail +2 | cut -f1 | xargs`
+	echo -n "$O2SIZE "
+
+	JLMSIZE=`size $kernel-jlm | tail +2 | cut -f1 | xargs`
+	echo "$JLMSIZE"
+done
