c48cd8eceb88a88cba9a0df2b4446921d24785f8
Nico Reissmann <nico.reissmann@gmail.com>
2017-10-17 12:42:43 +0200

    Allow to specify compilation targets as arguments to compile_all.sh script

--- compile_all.sh
+++ compile_all.sh
@@ -1,44 +1,54 @@
 #!	/bin/bash
 
+if [ "$#" -eq 0 ]; then
+	echo "No make targets specified."
+	exit 1
+fi
+
+TARGETS=""
+for TARGET in "$@"; do
+	TARGETS+=" $TARGET"
+done
+
 perl utilities/makefile-gen.pl .
 source ~/bin/load-jlm.sh
 
 # Datamining
-make -C datamining/correlation clean all
-make -C datamining/covariance clean all
+make -C datamining/correlation $TARGETS
+make -C datamining/covariance $TARGETS
 
 # Linear Algebra
-make -C linear-algebra/blas/gemm clean all
-make -C linear-algebra/blas/gemver clean all
-make -C linear-algebra/blas/gesummv clean all
-make -C linear-algebra/blas/symm clean all
-make -C linear-algebra/blas/syr2k clean all
-make -C linear-algebra/blas/syrk clean all
-make -C linear-algebra/blas/trmm clean all
+make -C linear-algebra/blas/gemm $TARGETS
+make -C linear-algebra/blas/gemver $TARGETS
+make -C linear-algebra/blas/gesummv $TARGETS
+make -C linear-algebra/blas/symm $TARGETS
+make -C linear-algebra/blas/syr2k $TARGETS
+make -C linear-algebra/blas/syrk $TARGETS
+make -C linear-algebra/blas/trmm $TARGETS
 
-make -C linear-algebra/kernels/2mm clean all
-make -C linear-algebra/kernels/3mm clean all
-make -C linear-algebra/kernels/atax clean all
-make -C linear-algebra/kernels/bicg clean all
-make -C linear-algebra/kernels/doitgen make all
-make -C linear-algebra/kernels/mvt clean all
+make -C linear-algebra/kernels/2mm $TARGETS
+make -C linear-algebra/kernels/3mm $TARGETS
+make -C linear-algebra/kernels/atax $TARGETS
+make -C linear-algebra/kernels/bicg $TARGETS
+make -C linear-algebra/kernels/doitgen $TARGETS
+make -C linear-algebra/kernels/mvt $TARGETS
 
-make -C linear-algebra/solvers/cholesky clean all
-make -C linear-algebra/solvers/durbin clean all
-make -C linear-algebra/solvers/gramschmidt clean all
-make -C linear-algebra/solvers/lu clean all
-make -C linear-algebra/solvers/ludcmp clean all
-make -C linear-algebra/solvers/trisolv clean all
+make -C linear-algebra/solvers/cholesky $TARGETS
+make -C linear-algebra/solvers/durbin $TARGETS
+make -C linear-algebra/solvers/gramschmidt $TARGETS
+make -C linear-algebra/solvers/lu $TARGETS
+make -C linear-algebra/solvers/ludcmp $TARGETS
+make -C linear-algebra/solvers/trisolv $TARGETS
 
 # Medley
-make -C medley/deriche clean all
-make -C medley/floyd-warshall clean all
-make -C medley/nussinov clean all
+make -C medley/deriche $TARGETS
+make -C medley/floyd-warshall $TARGETS
+make -C medley/nussinov $TARGETS
 
 #Stencils
-make -C stencils/adi clean all
-make -C stencils/fdtd-2d clean all
-make -C stencils/head-3d clean all
-make -C stencils/jacobi-1d clean all
-make -C stencils/jacobi-2d clean all
-make -C stencils/seidel-2d clean all
+make -C stencils/adi $TARGETS
+make -C stencils/fdtd-2d $TARGETS
+make -C stencils/head-3d $TARGETS
+make -C stencils/jacobi-1d $TARGETS
+make -C stencils/jacobi-2d $TARGETS
+make -C stencils/seidel-2d $TARGETS
