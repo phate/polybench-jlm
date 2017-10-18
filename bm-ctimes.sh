464ceaf2d0a010c45283013380354884402d1347
Nico Reissmann <nico.reissmann@gmail.com>
2017-10-18 14:16:58 +0200

    Provide compilation macros for computing compile times as arguments

--- bm-ctimes.sh
+++ bm-ctimes.sh
@@ -5,6 +5,11 @@ if [ -z "$JLMROOT" ]; then
 	exit 1
 fi
 
+OPTTIMES=""
+for OPT in "$@"; do
+	OPTTIMES+="-D${OPT} "
+done
+
 JIVEROOT=$JLMROOT/external/jive
 
 OPTFLAGS=`cat optflags`
@@ -12,13 +17,6 @@ echo "CC=clang-3.7" > config.mk
 echo "CPPFLAGS=-DPOLYBENCH_USE_C99_PROTO" >> config.mk
 echo "CFLAGS=-O0" >> config.mk
 echo "OPTFLAGS=$OPTFLAGS" >> config.mk
-
-OPTTIMES=""
-IFS=' ' read -ra OPTARRAY <<< "$OPTFLAGS"
-for OPT in "${OPTARRAY[@]}"; do
-	OPTTIME=`echo "$OPT" | cut -c 3- | awk '{print toupper($0)}'`
-	OPTTIMES+="-D${OPTTIME}TIME "
-done
 
 make -C $JIVEROOT clean
 make -C $JIVEROOT -j4 CFLAGS="-Wall --std=c++14 -xc++ -Wfatal-errors -O2"
